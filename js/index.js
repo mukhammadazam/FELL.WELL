b3c12cdda6c6a6a9afa063cada50f2547a46ee61
Mukhammad_Azam_001 <rizayevmuhammadazam9@gmail.com>
2023-02-03 06:06:21 +0300

    drabdawn continues

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ let navbar = document.querySelector(".nav");
 let list = document.querySelector(".nav__list");
 let list1 = document.querySelector("list1");
 let burger = document.querySelector(".nav__burger");
-burger.addEventListener("click", function(){
+burger.addEventListener("click", function () {
     list.classList.toggle("list1")
-})
+});
+
+
+let btn = document.querySelector(".click"),
+    list2 = document.querySelector(".click__list"),
+    list4 = document.querySelector(".liss");
+btn.addEventListener("click", function () {
+    list2.classList.toggle("click__list1")
+});
+
+
